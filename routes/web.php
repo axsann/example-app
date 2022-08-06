8ed5f186fdaffb292d6052aa24d528737325275f
axsann <7417697+axsann@users.noreply.github.com>
2022-08-06 20:22:44 +0900

    PsalmをLarastanに置き換え

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\ViewModels\WelcomeViewModel;
+use Illuminate\Support\Collection;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,3 +32,40 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+
+
+// ここから下はphpstanを試しに使っている
+
+Route::get('/hoge', function () {
+    hoge(10000);
+    $foo = fuga(collect([1, 2, 3, 4]));
+    print($foo);
+    $foo->each(function ($item) {
+        print($item);
+    });
+    hoge(3);
+    $vm = new WelcomeViewModel(1, $foo);
+    $hoge = $vm->list->map(function ($item) {
+        return $item;
+    });
+    print($hoge);
+    return view('welcome')
+        ->with('vm', $vm);
+});
+
+function hoge(int $x): int
+{
+    print($x);
+    return $x;
+}
+
+/**
+ * @param Collection<int, int> $list
+ * @return Collection<int, string>
+ */
+function fuga(Collection $list): Collection
+{
+    return $list->map(function (int $item) {
+        return strval($item);
+    });
+}
